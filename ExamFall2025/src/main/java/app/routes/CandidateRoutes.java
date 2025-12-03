6b4780edd4ead5f75944d73afb991acb688558a9
Phils1993 <phils1993@outlook.com>
2025-12-03 09:09:51 +0100

    copy CandidateRoutes info main

--- ExamFall2025/src/main/java/app/routes/CandidateRoutes.java
+++ ExamFall2025/src/main/java/app/routes/CandidateRoutes.java
@@ -1,46 +1,43 @@
 package app.routes;
 
 import app.controllers.CandidateController;
+import app.daos.CandidateDAO;
 import app.security.Roles;
-import app.services.CandidateService;
+import app.services.ServiceAPI;
 import io.javalin.apibuilder.EndpointGroup;
 import static io.javalin.apibuilder.ApiBuilder.*;
-import jakarta.persistence.EntityManagerFactory;
 
 public class CandidateRoutes {
 
     private final CandidateController controller;
+    private final ServiceAPI serviceAPI;
 
-    public CandidateRoutes(CandidateService candidateService) {
-        this.controller = new CandidateController(candidateService);
+    public CandidateRoutes(CandidateDAO candidateDAO,  ServiceAPI serviceAPI) {
+        this.controller = new CandidateController(candidateDAO);
+        this.serviceAPI = serviceAPI;
     }
 
     public EndpointGroup getRoutes() {
         return () -> {
             path("candidates", () -> {
-                // Use getAllCandidates which handles both:
-                // GET /candidates  (no query param) and
-                // GET /candidates?category={category} (filtered)
-                get(controller.getAllCandidates());
-
-                post(controller.create());
+                get(controller.getAll(), Roles.USER, Roles.ADMIN);
+                post(controller.create(), Roles.USER, Roles.ADMIN);
 
                 path("{id}", () -> {
-                    get(controller.getById());
-                    put(controller.update());
-                    delete(controller.delete());
+                    get(controller.getById(), Roles.USER, Roles.ADMIN);
+                    put(controller.update(), Roles.USER, Roles.ADMIN);
+                    delete(controller.delete(), Roles.USER, Roles.ADMIN);
                 });
 
                 path("{candidateId}/skills/{skillId}", () -> {
-                    put(controller.linkSkill());
-                    delete(controller.removeSkill()); // if you support removing via DELETE
+                    put(controller.linkSkill(), Roles.USER, Roles.ADMIN);
+                    delete(controller.removeSkill(), Roles.USER, Roles.ADMIN);
                 });
             });
 
-            // Reports namespace
             path("reports", () -> {
                 path("candidates", () -> {
-                    get("top-by-popularity", controller.getTopByPopularity());
+                    get("top-by-popularity", controller.getTopCandidateByPopularity(), Roles.USER, Roles.ADMIN);
                 });
             });
         };
